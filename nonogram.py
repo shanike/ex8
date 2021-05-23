33f6c6c2bd62ef04f4bb78a19a3413a9870a23fb
Shani Kehati <56911506+shanike@users.noreply.github.com>
2021-05-23 22:27:30 +0300

    autors and first function declaration

---
+++ nonogram.py
@@ -0,0 +1,8 @@
+def constraint_satisfactions (n, blocks):
+    """returns all possible filling for line of <n> int s according to <blocks> contraint
+        (list of lists)
+
+    Args:
+        n (int): length of row
+        blocks (int list): constraints
+    """
